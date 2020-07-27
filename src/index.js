f0fa0578b9c7f25d4c416624cf28b5a1ac10af57
Oloruntobi Olurombi <statemanui@gmail.com>
2020-07-27 11:53:45 +0100

    frontend done- backend will be done later

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+
+//Download ---man search for meaning Ebook!!!
+//There's two type of components in react namely 1. Custom Component 2. Simple Component 
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
